656738260cd8e5cc0d21d53724afc0442c2a507c
xudacheng <xudacheng@shouqianba.com>
2019-05-13 21:49:07 +0800

    improve

--- lianjia.sql
+++ lianjia.sql
@@ -1,12 +1,21 @@
-
--- set @communtiy = '合景叠翠峰';
--- set @communtiy = '尹山湖韵佳苑';
--- set @communtiy =  '保利悦玺';
-set @communtiy =  '保利居上';
+set @communtiy = '合景叠翠峰';
+set @communtiy = '尹山湖韵佳苑';
+set @communtiy = '保利悦玺';
+set @communtiy = '保利居上';
 
 -- 当前房源详细
 SELECT * FROM ershoufang.houseinfo 
 where community = @communtiy order by decoration desc,(unitPrice+0);
+
+-- 下架房源信息
+SELECT * FROM ershoufang.houseinfo
+WHERE
+    SUBSTR(validdate, 1, 10) NOT IN (SELECT 
+            SUBSTR(MAX(validdate), 1, 10)
+        FROM
+            ershoufang.houseinfo)
+ORDER BY validdate;
+
 
 -- 查询80平到100平的房源
 SELECT 
@@ -26,8 +35,21 @@ WHERE
         AND totalPrice * 10000 / unitPrice BETWEEN 80 AND 100
 ORDER BY square , decoration DESC , (unitPrice + 0);
 
+-- 价格发生变动的房源信息
+SELECT * FROM
+    (SELECT 
+        houseId, COUNT(*), GROUP_CONCAT(totalPrice)
+    FROM
+        ershoufang.hisprice
+    WHERE
+        community = @communtiy
+    GROUP BY houseId
+    HAVING COUNT(*) > 1) t
+        LEFT JOIN
+    houseinfo h ON t.houseId = h.houseId;
+
 -- 当前房源价格信息
-SELECT * FROM ershoufang.hisprice where community = @communtiy;
+SELECT * FROM ershoufang.hisprice where community = @communtiy order by houseId;
 
 -- 已售房源信息
 SELECT * FROM ershoufang.sellinfo where community = @communtiy order by dealdate desc;
